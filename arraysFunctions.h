8c79047ae3614fc20128c3a017c18c5841e2cfdd
weguns <wegunss@gmail.com>
2024-11-26 16:57:56 +0330

    making base files

---
+++ arraysFunctions.h
@@ -0,0 +1,8 @@
+//
+// Created by wegun on 11/26/2024.
+//
+
+#ifndef ARRAYSFUNCTIONS_H
+#define ARRAYSFUNCTIONS_H
+
+#endif //ARRAYSFUNCTIONS_H
---
+++ matrixFunctions.h
@@ -0,0 +1,8 @@
+//
+// Created by wegun on 11/26/2024.
+//
+
+#ifndef MATRIXFUNCTIONS_H
+#define MATRIXFUNCTIONS_H
+
+#endif //MATRIXFUNCTIONS_H
